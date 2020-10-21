ec30de550665f60c5e8ea671ea29ccb33c7cbd98
Felipe Blandow <felipeblandowsiqueira@gmail.com>
2020-10-21 17:35:38 -0300

    position class

---
+++ Board/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessConsole.Board
+{
+    class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+    }
+}
